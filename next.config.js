24c646804b9a2d7a20ba13a34a4a530a1645f3c9
nima <nima.nasrr@gmail.com>
2026-05-08 00:34:04 -0400

    Add all config files

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 }
-
-module.exports = nextConfig
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-}
-
 module.exports = nextConfig
