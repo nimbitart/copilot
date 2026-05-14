95767e6d888fffab96cb08883f8a34bb806cb6ce
nima <nima.nasrr@gmail.com>
2026-05-14 03:06:50 -0400

    Update pricing page text

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -240,7 +240,7 @@ export default function Pricing() {
         }}>
           Get Started
        </a>
-
+     </section>
       {/* Footer */}
       <footer style={{ 
         backgroundColor: '#333', 
